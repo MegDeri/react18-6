2f1d7b90b7547e1ea2c7dcdf3854f0e2043f5b9f
Magda Aderibigbe <magdaaderibigbe@gmail.com>
2019-05-03 22:37:54 +0300

    Create html and js files and modify them

---
+++ script.js
@@ -0,0 +1,64 @@
+var Counter = React.createClass({
+    getInitialState: function() {
+        return {
+            counter: 0
+        };
+    },
+
+    increment: function() {
+        this.setState({
+            counter: this.state.counter + 1
+        });
+    },
+
+    decrement: function() {
+        this.setState({
+            counter: this.state.counter - 1
+        });
+    },
+
+    render: function() {
+        return (React.createElement('div', {},
+            React.createElement('button', {onClick: this.increment}, 'Plus'),
+            React.createElement('button', {onClick: this.decrement}, 'Minus'),
+            React.createElement('span', {}, 'Licznik ' + this.state.counter)
+        ));
+    },
+
+    componentWillMount: function() {
+        console.log('Inint state before first render');
+        },
+    
+    componentDidMount: function() {
+        console.log('Query to API');
+    },
+    componentWillReceiveProps: function() {
+        console.log('Counting after update but befor render');
+    },
+
+    shouldComponentUpdate: function() {
+        console.log('Optimalization, return value and only if boolean is true goes to next one');
+        return true;
+    },
+
+    componentWillUpdate: function() {
+        console.log('Preparing to next state and return only then');
+    },
+
+    componentDidUpdate: function() {
+        console.log('Using on DOM');
+    },
+
+    componentWillUnmount: function() {
+        console.log('Delete some options as timers or listener');
+    }  
+    
+});
+
+
+var element = React.createElement('div', {},
+    React.createElement(Counter),
+    React.createElement(Counter),
+);
+
+ReactDOM.render(element, document.getElementById('app'));
